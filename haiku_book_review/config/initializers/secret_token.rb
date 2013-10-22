1fcaa59942c7ecff44d2e8112f71c5a6982fe793
Feather <lisafeather@gmail.com>
2013-10-21 18:23:30 -0700

    first commit

---
+++ haiku_book_review/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HaikuBookReview::Application.config.secret_key_base = '1e64f1bd635ec7e1ed93c7f6eda8305509cc51154afedc7aac692d0d44032220b3720fd68a765b3a293d9f4c32713c999df0481664593aba4ea16bdf4815c5b3'
---
+++ haiku_book_review/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HaikuBookReview::Application.config.session_store :cookie_store, key: '_haiku_book_review_session'
